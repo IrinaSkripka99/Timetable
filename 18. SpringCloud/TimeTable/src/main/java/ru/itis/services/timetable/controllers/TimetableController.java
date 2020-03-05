3314f03aa3894618ad0ca13436ce89b4dc2a8160
Dariya <dariyashagieva@gmail.com>
2020-03-05 23:43:02 +0300

    Микросервис

---
+++ 18. SpringCloud/TimeTable/src/main/java/ru/itis/services/timetable/controllers/TimetableController.java
@@ -0,0 +1,15 @@
+package ru.itis.services.timetable.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 07.04.2018
+ * VkUsersController
+ *
+ * @author Sidikov Marsel (First Software Engineering Platform)
+ * @version v1.0
+ */
+@RestController
+public class TimetableController {
+
+}
